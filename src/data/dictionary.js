8bfc0762abbffeccf959f70f145788f3875ce02b
tomrouet <rouettom@gmail.com>
2021-07-16 23:33:28 +0200

    Initialization

---
+++ src/data/dictionary.js
@@ -0,0 +1,35 @@
+/**
+ * @ContributorsList
+ * @Inateno / http://inateno.com / http://dreamirl.com
+ *
+ * this is the dictionary file sample that will be loaded by the project in the Localization module
+ * Please declare in the same way than this example.
+ */
+const dictionary = {
+  en: {
+    play: "Play",
+    settings: "Settings",
+    credits: "Credits",
+    pause: "Pause",
+    player: {
+      name: "Toto",
+      data: {
+        realname: "Super toto"
+      }
+    }
+  },
+  fr: {
+    play: "Jouer",
+    settings: "Options",
+    credits: "Credits",
+    pause: "Pause",
+    player: {
+      name: "Toto",
+      data: {
+        realname: "Super toto"
+      }
+    }
+  }
+};
+
+export default dictionary;
